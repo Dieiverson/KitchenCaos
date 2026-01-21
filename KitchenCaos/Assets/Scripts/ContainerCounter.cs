0fe17928dc4cfada00d353a07b6bb72f8b1f1ca0
Dieiverson Kava <38850216+Dieiverson@users.noreply.github.com>
2026-01-20 23:49:25 -0300

    Corrigindo bug ao interagir com container counter

--- KitchenCaos/Assets/Scripts/ContainerCounter.cs
+++ KitchenCaos/Assets/Scripts/ContainerCounter.cs
@@ -10,7 +10,7 @@ public class ContainerCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-        if (!HasKitchenObject())
+        if (!player.HasKitchenObject())
         {
             Transform kitchenObjectTransform = Instantiate(kitchenObjectsSO.prefab).transform;
             kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
