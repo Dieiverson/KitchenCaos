e6262289b16ca4f3b3906854d849a271274bf2be
Dieiverson Kava <38850216+Dieiverson@users.noreply.github.com>
2026-01-27 21:44:37 -0300

    Add Game Start

--- KitchenCaos/Assets/Scripts/Player.cs
+++ KitchenCaos/Assets/Scripts/Player.cs
@@ -49,6 +49,8 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     private void GameInput_OnInteractAlternateAction(object sender, EventArgs e)
     {
+       if (!GameManager.Instance.IsGamePlaying()) return;
+
         if (selectedCounter != null)
         {
             selectedCounter.InteractAlternate(this);
